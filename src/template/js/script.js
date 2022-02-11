707bfc85616a7ece5af2c4068c5da394f9c2bc6e
Andrey Myagkov <andreyoren@gmail.com>
2022-02-11 18:44:08 +0500

    Адаптация шапка, футер

---
+++ src/template/js/script.js
@@ -0,0 +1,10 @@
+/**
+ * Mobile panel
+ */
+const mp = new MobilePanel({
+    'navbar': '.nav-main'
+});
+
+mp.button({
+    'text': '<a href="/"><img src="/template/svg/logo-trustnavigator.svg" width="100" height="25" alt="Trust Navigator" class="logo-mobile__img"></a>',
+});
